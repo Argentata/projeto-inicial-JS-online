b133e317c864d118cf2ce8139fa455cad2a8feb7
Pedro <burlando@gmail.com>
2024-11-06 20:33:00 -0300

    projeto inicial JS

---
+++ app.js
@@ -0,0 +1,9 @@
+alert('Bienvenido al juego, "adivina el número secreto"');
+let NumeroSecreto = 30;
+
+num = prompt('Elija un número de 1 a 50');
+
+if (num < NumeroSecreto) {
+    alert('acertaste');
+
+} 
